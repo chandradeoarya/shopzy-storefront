6676ece0c3fdfbdf2e1717ab3ff2f7438ae34fe6
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-04-16 13:49:17 +0530

    burger toggle

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,21 @@
       <div class="navbar-brand">
         <router-link to="/" class="navbar-item"><strong>Djackets</strong></router-link>
 
-        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu">
+        <a class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar-menu" @click="showMobileMenu = !showMobileMenu">
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
         </a>
       </div>
-      <div class="navbar-menu" id="navbar-menu">
+      <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
         <div class="navbar-end">
           <router-link to="/summer" class="navbar-item">Summer</router-link>
           <router-link to="/winter" class="navbar-item">Winter</router-link>
         </div>
-      </div>
-      <div class="navbar-item">
-        <div class="buttons">
+        <div class="navbar-item">
+          <div class="buttons">
           <router-link to="/log-in" class="button is-light">Log in</router-link>
-
+          
           <router-link to="/cart" class="button is-success">
             <span class="icon">
               <i class="fas fa-shopping-cart"></i>
@@ -28,6 +27,7 @@
           </router-link>
         </div>
       </div>
+    </div>
     </nav>
   </div>
   <section class="section">
@@ -38,6 +38,17 @@
     <p class="has-text-centered">Copyright (c) 2021</p>
   </footer>
 </template>
+
+<script>
+  export default {
+    data(){
+      return {
+        showMobileMenu: false
+
+      }
+    }
+  }
+</script>
 
 <style lang="scss">
   @import '../node_modules/bulma';
