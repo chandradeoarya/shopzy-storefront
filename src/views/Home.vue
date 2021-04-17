70cc3fcdcfbc3dac8579f0d1b6c233a36e0dba3d
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-04-17 13:26:47 +0530

    added latest products homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,72 @@
 <template>
   <div class="home">
-    Home
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to checkroof
+        </p>
+        <p class="subtitle">
+          Discover what your roof can do for you
+        </p>
+      </div>
+    </section>
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest Products</h2>
+      </div>
+      <div class="column is-3" v-for="product in latestProducts" v:bind:key="product.id">
+        <div class="box">
+          <figure class="image mb-4">
+            <img v-bind:src="product.get_thumbnail" alt="">
+          </figure>
+
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">${{product.price}}</p>
+          
+          ViewDetails
+
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import axios from 'axios'
 export default {
+
   name: 'Home',
-  components: {
-    
+  data(){
+    return {
+      latestProducts:[]
+    }
+  },
+  components:{
+
+  },
+  mounted(){
+    this.getLatestProducts()
+  },
+  methods:{
+    getLatestProducts(){
+      axios.get('/api/v1/latest-products/')
+      .then((response)=>{
+        this.latestProducts = response.data
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+    }
   }
 }
 </script>
+
+<style scoped>
+  .image {
+    margin-top: -1.25rem;
+    margin-left: -1.25rem;
+    margin-right: -1.25rem;
+  }
+</style>
