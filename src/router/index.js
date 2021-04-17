c55d2b37396688c9ca75b5c50b8a2c06647d96fc
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-04-17 17:12:33 +0530

    added rainroof single product page

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../views/Home.vue";
+import Product from "@/views/Product";
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Home",
+    component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/:category_slug/:product_slug/",
+    name: "Product",
+    component: Product,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/Product.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="columns is-multiline">
+        <div class="column is-9">
+            <figure class="image is-16by9">
+                <img  v-bind:src="product.get_image" alt=""/>
+            </figure>
+            <h1 class="title">{{ product.name }}</h1>
+            <p>{{ product.description }}</p>
+        </div>
+        <div class="column is-3">
+            <h2 class="subtitle">Information</h2>
+            <p><strong>Price: </strong>{{product.price}}</p>
+            <div class="field has-addons mt-6">
+                <div class="control">
+                    <input type="number" class="input" min="1" v-model="quantity">
+                </div>
+            
+                <div class="control">
+                    <a class="button is-dark" @click="addToCart()">Add to cart</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+    export default{
+        name: 'Product',
+        data(){
+            return {
+                product:{},
+                quantity: 1
+            }
+        },
+        mounted(){
+            this.getProducts()
+        },
+        methods:{
+            getProducts(){
+                const category_slug = this.$route.params.category_slug
+                const product_slug = this.$route.params.product_slug
+                axios.get(`/api/v1/products/${category_slug}/${product_slug}`)
+                .then((response)=>{
+                     this.product = response.data
+                })
+                .catch((error)=>{
+                    console.log(error)
+                })
+            }
+        }
+    }
+</script>
