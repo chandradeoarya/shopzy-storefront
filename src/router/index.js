a445f7efdd0d32e95f216b9355d1ec2cad67fb8c
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-04-18 18:23:28 +0530

    added checkout page, router, stripe.js, stripe card no. field

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Cart from "../views/Cart.vue";
 import SignUp from "../views/SignUp.vue";
 import LogIn from "../views/LogIn.vue";
 import MyAccount from "../views/MyAccount.vue";
+import Checkout from "../views/Checkout.vue";
 
 import store from "../store";
 const routes = [
@@ -46,6 +47,15 @@ const routes = [
     path: "/cart",
     name: "Cart",
     component: Cart,
+  },
+
+  {
+    path: "/cart/checkout",
+    name: "Checkout",
+    component: Checkout,
+    meta: {
+      requireLogin: true,
+    },
   },
   {
     path: "/:category_slug/:product_slug/",
