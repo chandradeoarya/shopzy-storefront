deddf12eae7a5f6a9aa4e8f3948e5c8d9fe94e46
chandradeoarya <chandradeoaryaarya@gmail.com>
2021-06-04 18:53:47 +0530

    env base url

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://3616dfec6f59.ngrok.io";
+axios.defaults.baseURL = "https://4334a29903ec.ngrok.io";
 
 createApp(App)
   .use(store)
